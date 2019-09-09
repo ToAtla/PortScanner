2a359c557ed91ca3112f60d50ec1654737e8b983
Þórður Atlason <thorduratl17@ru.is>
2019-09-09 16:52:46 +0900

    Checksum implemented from Unix book

--- scanner.cpp
+++ scanner.cpp
@@ -16,23 +16,55 @@
 
 #include <arpa/inet.h>
 #include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
+#include "icmp.h"
+//#include <netinet/ip_icmp.h>
 
 
 #define PORT	4002
 #define MAXLINE	1024
-
+/*
 struct icmphdr
 {
-	char type = 0;
-	char code = 0;
-	short checksum = 0;
-	short identifier = 0;
-	short sequence_number = 0;
+	char type;
+	char code;
+	short checksum;
+	short identifier;
+	short sequence_number;
 };
+*/
 
 
-void scan_ports()
+// Function taken from "Unix Network Programming 2nd Edition Volume 1 - Richard Stevens"
+unsigned short in_cksum(unsigned short *addr, int len)
+{
+  int nleft = len;
+  int sum = 0;
+  unsigned short *w = addr;
+  unsigned short answer = 0;
+
+  /*
+   * Our algorithm is simple, using a 32 bit accumulator (sum), we add
+   * sequential 16 bit words to it, and at the end, fold back all the
+   * carry bits from the top 16 bits into the lower 16 bits.
+  */
+  while (nleft > 1) {
+    sum += *w++;
+    nleft -= 2;
+  }
+    /* mop up an odd byte, if necessary */
+  if (nleft == 1){
+    *(unsigned char *) (&answer) = *(unsigned char *) w;
+    sum += answer;
+  }
+    /* add back carry outs from top 16 bits to low 16 bits */
+  sum = (sum >> 16) + (sum & 0xffff);   /* add hi 16 to low 16*/
+  sum += (sum >> 16);     /* add carry */
+  answer = ~sum;          /* truncate to 16 bits */
+  return (answer);
+}
+
+
+void send_ping()
 {
 	// socket
 	int sockfd = 0;
@@ -40,10 +72,16 @@ void scan_ports()
 	struct sockaddr_in servaddr;
 	char buffer[MAXLINE];
 
-	struct icmphdr *icp = new icmphdr;
-	icp->type = 8; // ICMP_ECHO
-	icp->code = 0;
-	icp->checksum = 0xfff7; // Hardcoded
+	struct icmphdr icp;
+	int icmp_length = 8; // 8 byte long header with no attached data
+	bzero(&icp, sizeof(icp));
+
+	icp.type = ICMP_ECHO;
+	icp.code = 0;
+	icp.checksum = 0;
+	icp.un.echo.id = getpid();
+	icp.un.echo.sequence = 0;
+	icp.checksum = in_cksum((u_short *) &icp, icmp_length); //0xbff7; // Hardcoded
 	printf("icp created\n");
 
 	if ( (sockfd  = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0 )
@@ -62,22 +100,27 @@ void scan_ports()
 		return;
 	}
 
-	int n;
-	socklen_t len = sizeof servaddr;
+
 
 	// sendto
-	sendto(sockfd, icp, cc, 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+	sendto(sockfd, &icp, cc, 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
   printf("Echo sent.\n");
 
-  // recvfrom
+	int n;
+	socklen_t len = sizeof servaddr;
   n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
 
   printf("Server : %i bytes recieved\n", n);
 
   close(sockfd);
 
+}
 
+void recieve_reply(){
+	// recvfrom
+	/*
 
+	*/
 }
 
 int main(int argc, char const *argv[])
@@ -94,7 +137,8 @@ int main(int argc, char const *argv[])
 	int high_port = atoi(argv[3]);
 	*/
 
-	scan_ports();
+	send_ping();
+	//recieve_reply();
 	// printf("No ports were scanned\n");
 
 	return 0;
