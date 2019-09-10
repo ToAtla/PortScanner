4026f619e89e7f3ff481b8dd0296c6c0f0c41db7
doddi321 <thordurf17@ru.is>
2019-09-10 12:02:18 +0000

    byrjaði a minu implementationi

---
+++ scanner.cpp
@@ -0,0 +1,190 @@
+//
+// Simple scanner application for Project 2 TSAM-409
+//
+// Author: thorduratl17@ru.is and thordurf17@ru.is
+//
+#include <iostream>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include "icmp.h"
+//#include <netinet/ip_icmp.h>
+#include <netinet/udp.h>
+
+//#define PORT	4002
+#define MAXLINE 1024
+
+const char *ip_address = "130.208.243.61";
+const int local_port = 39123;
+
+// Function taken from "Unix Network Programming 2nd Edition Volume 1 - Richard Stevens"
+unsigned short in_cksum(unsigned short *addr, int len)
+{
+	int nleft = len;
+	int sum = 0;
+	unsigned short *w = addr;
+	unsigned short answer = 0;
+
+	/*
+   * Our algorithm is simple, using a 32 bit accumulator (sum), we add
+   * sequential 16 bit words to it, and at the end, fold back all the
+   * carry bits from the top 16 bits into the lower 16 bits.
+  */
+	while (nleft > 1)
+	{
+		sum += *w++;
+		nleft -= 2;
+	}
+	/* mop up an odd byte, if necessary */
+	if (nleft == 1)
+	{
+		*(unsigned char *)(&answer) = *(unsigned char *)w;
+		sum += answer;
+	}
+	/* add back carry outs from top 16 bits to low 16 bits */
+	sum = (sum >> 16) + (sum & 0xffff); /* add hi 16 to low 16*/
+	sum += (sum >> 16);					/* add carry */
+	answer = ~sum;						/* truncate to 16 bits */
+	return (answer);
+}
+
+void send_ping(int port)
+{
+	// socket
+	int sockfd = 0;
+	int cc = 8;
+	struct sockaddr_in servaddr;
+	char buffer[MAXLINE];
+
+	struct icmphdr icp;
+	int icmp_length = 8; // 8 byte long header with no attached data
+	bzero(&icp, sizeof(icp));
+
+	icp.type = ICMP_ECHO;
+	icp.code = 0;
+	icp.checksum = 0;
+	icp.un.echo.id = getpid();
+	icp.un.echo.sequence = 0;
+	icp.checksum = in_cksum((u_short *)&icp, icmp_length); //0xbff7; // Hardcoded
+	printf("icp created\n");
+
+	if ((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
+	{
+		printf("Socket creation failed. Root privilages may be missing.\n");
+		return;
+	};
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port);
+	// Converts the IPv4 address from text to binary form
+
+	if (inet_pton(AF_INET, ip_address, &servaddr.sin_addr) <= 0)
+	{
+		printf("\nInvalid address/ Address not supported \n");
+		return;
+	}
+
+	// sendto
+	sendto(sockfd, &icp, cc, 0, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+	printf("Echo sent.\n");
+
+	int n;
+	socklen_t len = sizeof servaddr;
+	n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
+
+	printf("Server : %i bytes recieved\n", n);
+
+	close(sockfd);
+}
+
+void send_raw_udp(int port)
+{
+	int sockfd;
+	struct sockaddr_in servaddr;
+	char recvline[1025];
+	char datagram[1024];
+	memset(datagram, 0, 1024);
+	char *sendline;
+	bzero(&servaddr, sizeof(servaddr));
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port);
+	if (inet_pton(AF_INET, ip_address, &servaddr.sin_addr) <= 0)
+	{
+		printf("\nInvalid address/ Address not supported \n");
+		return;
+	}
+	if ((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP)) < 0)
+	{
+		printf("Socket creation failed. Root privilages may be missing.\n");
+		return;
+	};
+
+	struct udphdr *udph = (struct udphdr *)datagram;
+
+	//bzero(&udph, sizeof(udph));
+	udph->uh_sport = htons(local_port);
+	udph->uh_dport = htons(port);
+	sendline = datagram + sizeof(struct udphdr);
+	strcpy(sendline, "Hello");
+	udph->uh_ulen = htons(8 + strlen(sendline));
+	udph->uh_sum = 0;
+	int udp_packet_length = sizeof(struct udphdr) + strlen(sendline);
+	if ((sendto(sockfd, udph, udp_packet_length, 0, (const struct sockaddr *)&servaddr, sizeof(servaddr))) < 0)
+	{
+		printf("Sending failed\n");
+		return;
+	}
+	printf("Sent empty UDP\n");
+	socklen_t len = sizeof servaddr;
+
+	int n = recvfrom(sockfd, (char *)recvline, 1024, 0, (struct sockaddr *)&servaddr, &len);
+	recvline[n] = '\0';
+	printf("Recieved %d bytes\n", n);
+	fputs(recvline, stdout);
+	/**/
+}
+
+void recieve_reply()
+{
+	// recvfrom
+	/*
+
+	*/
+}
+
+int main(int argc, char const *argv[])
+{
+	// Input code
+	/*
+	if(argc != 4)
+	{
+			printf("Usage: ./scanner <ip_address> <low_start> <high_end>\n");
+			exit(0);
+	}
+ 	const char* ip_address = argv[1];
+	int low_port = atoi(argv[2]);
+	int high_port = atoi(argv[3]);
+	*/
+
+	/*
+	for (int i = 4000; i < 4101; i++) {
+		send_ping(i);
+	}
+	*/
+	//send_ping(4095);
+	send_raw_udp(4026);
+	//recieve_reply();
+	// printf("No ports were scanned\n");
+
+	return 0;
+}
--- scanner_doddi.cpp
+++ scanner_doddi.cpp
@@ -5,54 +5,79 @@
 
 using namespace std;
 
-int scan_ports(char* ip_address, int low_port, int high_port)
+// scans UDP ports for a given address and a given port range.
+int scan_ports(char *ip_address, int low_port, int high_port)
 {
-	// TODO: what is ther difference between socket_raw and sock_dgram and which should we use?
-	// TODO: why are we using udp instead of tcp?
-	int socketfd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
-    if (socketfd < 0)
-    {
-        perror("Failed to open socket");
-        return (-1);
-    }
 
-	struct sockaddr_in server_socket_addr;  					// address of server
+	// socket to send to udp ports with IPv4 protocol
+	int sendSocketFd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sendSocketFd < 0)
+	{
+		perror("Failed to open send socket");
+		return (-1);
+	}
+
+	// socket to receive from server if port is open
+	int recvSocketFd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+	if (recvSocketFd < 0)
+	{
+		perror("Failed to open recv socket");
+		return (-1);
+	}
+
+	struct sockaddr_in server_socket_addr;						// address of server
 	memset(&server_socket_addr, 0, sizeof(server_socket_addr)); // Initialise memory
-	server_socket_addr.sin_family = AF_INET;                    // pv4
-    server_socket_addr.sin_addr.s_addr = inet_addr(ip_address); // bind to server ip
+	server_socket_addr.sin_family = AF_INET;					// pv4
+	server_socket_addr.sin_addr.s_addr = inet_addr(ip_address); // bind to server ip
 
 	// iterate through each portnumber in the range given
-	for (int portno = low_port; portno <= high_port; portno++) {
+	for (int portno = low_port; portno <= high_port; portno++)
+	{
 		server_socket_addr.sin_port = htons(portno); // portnumber
 
 		// connect to server
-		if (connect(socketfd, (struct sockaddr *)&server_socket_addr, sizeof(server_socket_addr)) < 0)
+		if (connect(sendSocketFd, (struct sockaddr *)&server_socket_addr, sizeof(server_socket_addr)) == 0)
 		{
-			cout << "failed to connect to server on port: " << portno << endl;
+			string sendString = "knock";
+
+			// send the server the command
+			if (send(sendSocketFd, sendString.c_str(), sendString.size() + 1, 0) >= 0)
+			{
+				int responseSize = 6000;
+				char response[responseSize];
+				memset(response, 0, responseSize);						   // zero initialize char array
+				int byteCount = recv(recvSocketFd, response, responseSize, 0); // this blocks and waits til it receives something from the server
+				if (byteCount < 0)
+				{
+					cout << "error receiving output from server" << endl;
+				}
+				else
+				{
+					//response[byteCount] = '\0'; // make sure to end the string at the right spot so we dont read of out memory
+					fputs(response, stdout);
+					cout << "Byte count received: " << byteCount << ", " << "on port: " << portno << endl;
+				}
+			}
 		}
-
-
 	}
 
 	return 1;
-
-
 }
 
 int main(int argc, char *argv[])
 {
-	if(argc != 4)
+	if (argc != 4)
 	{
 		printf("Usage: ./scanner <ip_address> <low_start> <high_end>\n");
 		exit(0);
 	}
- 	
- 	char* ip_address = argv[1];
+
+	char *ip_address = argv[1];
 	int low_port = atoi(argv[2]);
 	int high_port = atoi(argv[3]);
-	
-	if (scan_ports(ip_address, low_port, high_port) < 0) {
 
+	if (scan_ports(ip_address, low_port, high_port) < 0)
+	{
 	};
 
 	return 0;
